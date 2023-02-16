6162b042691b59912bacd7d349da7807ff861769
CorinaVizitiu <corina.vizitiu@yahoo.com>
2023-02-16 09:23:49 +0100

    23

--- src/Pages/Galerie.js
+++ src/Pages/Galerie.js
@@ -4,14 +4,14 @@ import './Galerie.css';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
- import img1 from '../img/imag1.jpg';
- import img2 from '../img/img2.jpeg';
- import img3 from '../img/img3.jpg';
- import img4 from '../img/img4.jpg';
- import img5 from '../img/img5.jpg';
- import img6 from '../img/img6.jpg';
- import img7 from '../img/img7.jpg';
- import img8 from '../img/img8.jpg';
+import img1 from '../img/imag1.jpg';
+import img2 from '../img/img2.jpeg';
+import img3 from '../img/img3.jpg';
+import img4 from '../img/img4.jpg';
+import img5 from '../img/img5.jpg';
+import img6 from '../img/img6.jpg';
+import img7 from '../img/img7.jpg';
+import img8 from '../img/img8.jpg';
 
 
 function App() {
@@ -37,21 +37,21 @@ function App() {
   };
   return (
     <div className="App">
-           <Menu />
-   <h1>Gallerie</h1>
-   
+      <Menu />
+      <h1>Gallerie</h1>
+
       <Carousel responsive={responsive}>
         <div>
           <img src={img1} alt="" />
-      
+
         </div>
         <div>
           <img src={img2} alt="" />
-     
+
         </div>
         <div>
           <img src={img3} alt="" />
-       
+
         </div>
         <div>
           <img src={img4} alt="" />
@@ -59,21 +59,21 @@ function App() {
         </div>
         <div>
           <img src={img5} alt="" />
-     
+
         </div>
         <div>
           <img src={img6} alt="" />
-         
+
         </div>
         <div>
           <img src={img7} alt="" />
-      
+
         </div>
         <div>
           <img src={img8} alt="" />
-        
+
         </div>
-      
+
       </Carousel>
     </div>
   );
